24032814c88f831af563d8415b1e70e6c5ec540f
David Bolin <david@bolin.at>
2019-05-14 11:28:13 -0700

    solved the tower

--- tower.js
+++ tower.js
@@ -1,45 +1,14 @@
 'use strict';
 
-function isLegal(move, structure) {
-  // move = ['A', 'B']
-  if (structure[move[0]].length !== 0) {
-    if (structure[move[1]].length === 0 || structure[move[1]][0] > structure[move[0]][0]) {
-      return true;
-    }
+function solveTower(numDisks, A, B, C) {
+
+  if (numDisks === 1) {
+    return [[A, C]];
   }
 
-  return false;
+  let intermediateSol = solveTower(numDisks - 1, A, C, B);
+  intermediateSol.push([A, C]);
+  return intermediateSol.concat(solveTower(numDisks - 1, B, A, C));
 }
 
-function solveTower(structure, numDisks) {
-  let solution = [];
-  const numRods = structure.length;
-  const possibleMoves = [
-    ['A', 'B'], ['A', 'C'], ['B', 'C'], ['B', 'A'], ['C', 'A'], ['C', 'B']
-  ];
-
-  if (structure.C.length === numDisks) {
-    return solution;
-  }
-
-  possibleMoves.forEach(move => {
-    if (isLegal(move, structure)) {
-      const sourceArr = structure[move[0]];
-      const destArr = structure[move[1]];
-      
-      destArr.unshift(sourceArr.shift());
-      
-
-      return solveTower()
-    }
-  });
-
-}
-
-const testTower = {
-  A: [1, 2, 3],
-  B: [],
-  C: []
-};
-
-console.log(solveTower(testTower, 3));
+console.log(solveTower(7, 'A', 'B', 'C'));
